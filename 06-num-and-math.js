d75fdaa4f510761aef6c2e61eed898c32f17d803
Nikhil Bhor <136161109+MrCoder420@users.noreply.github.com>
2025-07-24 14:03:43 +0000

    g

---
+++ 06-num-and-math.js
@@ -0,0 +1,28 @@
+// const score = 300;
+
+//  console.log(score)
+
+
+//  balance = new Number(1000)
+// console.log(typeof balance.toString())
+//  console.log(balance.toFixed(2))
+
+
+let hun = 10000
+
+console.log(Math.abs(-4))
+console.log(Math.random())
+console.log(Math.random()*10)
+
+
+
+const  min=10
+
+const max =20
+
+Math.random()
+
+
+
+
+
---
+++ variale.js
@@ -0,0 +1,19 @@
+"use strict " // treat all js as  never version
+
+console.log("hello");
+
+// alert(3+3) using node js not browser
+
+console.log(3+3)
+
+// data type 
+
+let name = " hitesh "
+ let age = 18
+ let isLoggedIn = false
+
+ /* number =2
+
+ null - stand aone value
+
+ undefined -- 
